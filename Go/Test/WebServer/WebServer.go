6a046472ce8bdb837b3bddfd7fca6af7e3cde5e9
ady28 <dumitras.adrian@yahoocom>
2021-12-17 16:29:22 +0200

    Add files via upload

--- Go/Test/WebServer/WebServer.go
+++ Go/Test/WebServer/WebServer.go
@@ -3,76 +3,154 @@ package main
 import (
 	"context"
 	"fmt"
-	"net/http"
+	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type Post struct {
+	ID    primitive.ObjectID `bson:"_id,omitempty"`
+	Title string             `bson:"title,omitempty"`
+	Body  string             `bson:"body,omitempty"`
+}
+
+var mongoDBServerName = "localhost"
+var mongoDBServerPort = "27017"
 
 //run go mod init and go mod tidy
 
 func main() {
 
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	ctx, ctxc := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer client.Disconnect(ctx)
-	defer ctxc()
-	databases, err := client.ListDatabaseNames(ctx, bson.M{})
-	if err != nil {
-		fmt.Println(err)
-	}
+	databases := ListDatabases(mongoDBServerName, mongoDBServerPort)
 	fmt.Println(databases)
+	collections := ListCollections(mongoDBServerName, mongoDBServerPort)
+	fmt.Println(collections)
+	//newPost(mongoDBServerName, mongoDBServerPort)
+	//findPost(mongoDBServerName, mongoDBServerPort, "Adi1")
+	//deletePost(mongoDBServerName, mongoDBServerPort, "Adi1")
+	findPost(mongoDBServerName, mongoDBServerPort, "61bbbd14f24141e64eb8648a")
+	//getAllPosts(mongoDBServerName, mongoDBServerPort)
 
-	type Post struct {
-		Title string `bson:"title,omitempty"`
-		Body  string `bson:"body,omitempty"`
-	}
-
-	cols, _ := client.Database("testdb").ListCollectionNames(ctx, bson.D{})
-	fmt.Println(cols)
-	collection := client.Database("testdb").Collection("testc")
-	filter := bson.D{}
-	c, _ := collection.CountDocuments(ctx, filter)
-	fmt.Println(c)
-	//docs := []interface{}{
-	//	bson.D{{"title", "World"}, {"body", "Hello World"}},
-	//	bson.D{{"title", "Mars"}, {"body", "Hello Mars"}},
-	//	bson.D{{"title", "Pluto"}, {"body", "Hello Pluto"}},
-	//}
-	//res, insertErr := collection.InsertMany(ctx, docs)
-	//if insertErr != nil {
-	//	fmt.Println(insertErr)
-	//}
-	//fmt.Println(res)
-
-	cur, currErr := collection.Find(ctx, bson.D{})
-
-	if currErr != nil {
-		panic(currErr)
-	}
-	defer cur.Close(ctx)
-
-	var posts []Post
-	if err = cur.All(ctx, &posts); err != nil {
-		panic(err)
-	}
-	fmt.Println(posts)
-
-	//http.HandleFunc("/", sroot)
-	//http.ListenAndServe(":8080", nil)
 }
 
-func sroot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Adi GoLang web server")
+func MongoDBConnect(server, port string) (*mongo.Client, context.Context, context.CancelFunc) {
+	mongoString := "mongodb://" + server + ":" + port
+	ctx, ctxCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoString))
+	if err != nil {
+		panic(err)
+	}
+
+	return client, ctx, ctxCancel
+}
+
+func ListDatabases(server, port string) []string {
+
+	client, ctx, ctxCancel := MongoDBConnect(server, port)
+
+	defer client.Disconnect(ctx)
+	defer ctxCancel()
+
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		panic(err)
+	}
+
+	return databases
+}
+
+func ListCollections(server, port string) []string {
+
+	client, ctx, ctxCancel := MongoDBConnect(server, port)
+
+	defer client.Disconnect(ctx)
+	defer ctxCancel()
+
+	collections, err := client.Database("testdb").ListCollectionNames(ctx, bson.D{})
+	if err != nil {
+		panic(err)
+	}
+
+	return collections
+}
+
+func newPost(server, port string) {
+	client, ctx, ctxCancel := MongoDBConnect(server, port)
+
+	defer client.Disconnect(ctx)
+	defer ctxCancel()
+
+	post := Post{
+		Title: "Adi1",
+		Body:  "Adi1 body",
+	}
+
+	collection := client.Database("testdb").Collection("testc")
+	_, err := collection.InsertOne(ctx, post)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func findPost(server, port, title string) {
+
+	var post Post
+
+	client, ctx, ctxCancel := MongoDBConnect(server, port)
+
+	defer client.Disconnect(ctx)
+	defer ctxCancel()
+
+	collection := client.Database("testdb").Collection("testc")
+
+	ID, err := primitive.ObjectIDFromHex(title)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := collection.FindOne(ctx, bson.M{"_id": ID}).Decode(&post); err != nil {
+		fmt.Printf("%s not found\n", title)
+	} else {
+		fmt.Println(post.ID.Hex())
+	}
+}
+
+func getAllPosts(server, port string) {
+	var posts []Post
+
+	client, ctx, ctxCancel := MongoDBConnect(server, port)
+
+	defer client.Disconnect(ctx)
+	defer ctxCancel()
+
+	collection := client.Database("testdb").Collection("testc")
+
+	filterCursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = filterCursor.All(ctx, &posts); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(posts)
+}
+
+func deletePost(server, port, title string) {
+	client, ctx, ctxCancel := MongoDBConnect(server, port)
+
+	defer client.Disconnect(ctx)
+	defer ctxCancel()
+
+	collection := client.Database("testdb").Collection("testc")
+
+	result, err := collection.DeleteOne(ctx, bson.M{"title": title})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("DeleteOne removed %v document(s)\n", result.DeletedCount)
 }
--- Go/Test/WebServer2/ws.go
+++ Go/Test/WebServer2/ws.go
@@ -1,34 +1,41 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/gorilla/mux"
 )
 
-type Article struct {
-	Id      string `json:"Id"`
-	Title   string `json:"Title"`
-	Desc    string `json:"Desc"`
-	Content string `json:"Content"`
+type Post struct {
+	ID    primitive.ObjectID `bson:"_id,omitempty"`
+	Title string             `bson:"title,omitempty"`
+	Body  string             `bson:"body,omitempty"`
 }
 
-var Articles []Article
-
+var mongoDBServerName = os.Getenv("MONGODBSERVERNAME")
+var mongoDBServerPort = os.Getenv("MONGODBSERVERPORT")
 var PORT = os.Getenv("PORT")
 
 //run go mod init and go mod tidy
 
 func main() {
 
-	Articles = []Article{
-		{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-	}
+	databases := ListDatabases(mongoDBServerName, mongoDBServerPort)
+	fmt.Println(databases)
+	collections := ListCollections(mongoDBServerName, mongoDBServerPort)
+	fmt.Println(collections)
 
 	handleRequests()
 }
@@ -36,12 +43,12 @@ func main() {
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", sroot)
-	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/posts", getAllPosts)
 	//the delete method must be first otherwise the get one will be executed
-	myRouter.HandleFunc("/article", updateArticle).Methods("PUT")
-	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
-	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/post/{id}", updatePostBody).Methods("PUT")
+	myRouter.HandleFunc("/post/{id}", deletePost).Methods("DELETE")
+	myRouter.HandleFunc("/post/{id}", findPost)
+	myRouter.HandleFunc("/post", newPost).Methods("POST")
 	http.ListenAndServe(":"+PORT, myRouter)
 }
 
@@ -49,50 +56,162 @@ func sroot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the Adi GoLang web server")
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(Articles)
+func MongoDBConnect() (*mongo.Client, context.Context, context.CancelFunc) {
+	mongoString := "mongodb://" + mongoDBServerName + ":" + mongoDBServerPort
+	ctx, ctxCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoString))
+	if err != nil {
+		panic(err)
+	}
+
+	return client, ctx, ctxCancel
 }
 
-func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+func ListDatabases(server, port string) []string {
+
+	client, ctx, ctxCancel := MongoDBConnect()
+
+	defer client.Disconnect(ctx)
+	defer ctxCancel()
+
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		panic(err)
+	}
+
+	return databases
+}
+
+func ListCollections(server, port string) []string {
+
+	client, ctx, ctxCancel := MongoDBConnect()
+
+	defer client.Disconnect(ctx)
+	defer ctxCancel()
+
+	collections, err := client.Database("testdb").ListCollectionNames(ctx, bson.D{})
+	if err != nil {
+		panic(err)
+	}
+
+	return collections
+}
+
+func newPost(w http.ResponseWriter, r *http.Request) {
+	client, ctx, ctxCancel := MongoDBConnect()
+
+	defer client.Disconnect(ctx)
+	defer ctxCancel()
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var post Post
+	json.Unmarshal(reqBody, &post)
+
+	collection := client.Database("testdb").Collection("testc")
+	_, err := collection.InsertOne(ctx, post)
+	if err != nil {
+		panic(err)
+	}
+
+	json.NewEncoder(w).Encode(post)
+}
+
+func findPost(w http.ResponseWriter, r *http.Request) {
+
 	vars := mux.Vars(r)
 	key := vars["id"]
-	for _, article := range Articles {
-		if article.Id == key {
-			json.NewEncoder(w).Encode(article)
-		}
+
+	id, err := primitive.ObjectIDFromHex(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var post Post
+
+	client, ctx, ctxCancel := MongoDBConnect()
+
+	defer client.Disconnect(ctx)
+	defer ctxCancel()
+
+	collection := client.Database("testdb").Collection("testc")
+
+	if err := collection.FindOne(ctx, bson.M{"_id": id}).Decode(&post); err != nil {
+		fmt.Printf("%s not found\n", id)
+	} else {
+		json.NewEncoder(w).Encode(post)
 	}
 }
 
-func createNewArticle(w http.ResponseWriter, r *http.Request) {
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	var article Article
-	json.Unmarshal(reqBody, &article)
+func getAllPosts(w http.ResponseWriter, r *http.Request) {
+	var posts []Post
 
-	Articles = append(Articles, article)
-	json.NewEncoder(w).Encode(article)
+	client, ctx, ctxCancel := MongoDBConnect()
+
+	defer client.Disconnect(ctx)
+	defer ctxCancel()
+
+	collection := client.Database("testdb").Collection("testc")
+
+	filterCursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = filterCursor.All(ctx, &posts); err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(posts)
 }
 
-func deleteArticle(w http.ResponseWriter, r *http.Request) {
+func deletePost(w http.ResponseWriter, r *http.Request) {
+
 	vars := mux.Vars(r)
-	id := vars["id"]
-	for index, article := range Articles {
-		if article.Id == id {
-			Articles = append(Articles[:index], Articles[index+1:]...)
-		}
+	key := vars["id"]
+
+	id, err := primitive.ObjectIDFromHex(key)
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	client, ctx, ctxCancel := MongoDBConnect()
+
+	defer client.Disconnect(ctx)
+	defer ctxCancel()
+
+	collection := client.Database("testdb").Collection("testc")
+
+	result, err := collection.DeleteOne(ctx, bson.M{"_id": id})
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(result)
 }
 
-func updateArticle(w http.ResponseWriter, r *http.Request) {
+func updatePostBody(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	var article Article
-	json.Unmarshal(reqBody, &article)
-	for index, art := range Articles {
-		if art.Id == article.Id {
-			Articles[index].Content = article.Content
-			Articles[index].Desc = article.Desc
-			Articles[index].Title = article.Title
-		}
+	var body string
+	json.Unmarshal(reqBody, &body)
+
+	id, err := primitive.ObjectIDFromHex(key)
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	client, ctx, ctxCancel := MongoDBConnect()
+
+	defer client.Disconnect(ctx)
+	defer ctxCancel()
+
+	collection := client.Database("testdb").Collection("testc")
+	filter := bson.M{"_id": bson.M{"$eq": id}}
+	update := bson.M{"$set": bson.M{"body": body}}
+	updated, err := collection.UpdateOne(ctx, filter, update)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(updated)
 }
